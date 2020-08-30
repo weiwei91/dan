926dbb60ebcdb56854ff0b168390fcdf771385f8
weiwei <weismart1@gmail.com>
2020-08-30 16:27:01 +0800

    初始化提交

---
+++ src/test/java/com/smart/dan/DanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart.dan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
